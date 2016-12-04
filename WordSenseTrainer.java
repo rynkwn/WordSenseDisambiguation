418904b87926f804b9d6e6ba8dc9bd19b51a2ded
Ryan H Kwon <17rhk1@salers.cs.williams.edu>
2016-12-04 14:12:21 -0500

    Minor cleanup changes to WordSenseTrainer.

--- WordSenseTrainer.java
+++ WordSenseTrainer.java
@@ -1,17 +1,19 @@
-
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class WordSenseTrainer {
 
-	// Constructor that does training.
-	// Reading in the corpus and tokenizing it.
-	// Doing random indexing, constructing the sentence mapping.
-
-	
-	// Retrieve list of sentences that use this word ranked by
-	// closest word sense.
-	public ArrayList<String> retrieve();
-
-	
-	// Score a sentence based on "similarity" with original sentence.
-	public double score(String sent1, String sent2, String word);
+    // Constructor that does training.
+    // Reading in the corpus and tokenizing it.
+    // Doing random indexing, constructing the sentence mapping.
+    public WordSenseTrainer();
+    
+    
+    // Retrieve list of sentences that use this word ranked by
+    // closest word sense.
+    public ArrayList<String> retrieve(String inputSentence, String word);
+    
+    
+    // Score a sentence based on "similarity" with original sentence.
+    public double score(String sentence, String inputSentence, String word);
 }
