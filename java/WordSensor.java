ac762a651bec2aa28b13b5578b4985cb10e19153
rkwon <rynkwn@gmail.com>
2016-12-07 18:48:37 -0500

    Created brief scripts to run the program on the whole data set, and to run on the test data set. Filling out WordSensor.

--- java/WordSensor.java
+++ java/WordSensor.java
@@ -1,5 +1,37 @@
 
 
+// The interface for WordSenseTrainer
 public class WordSensor {
-	
+
+    public static void main(String[] args) {
+
+	// FLAG: Set to false if we should stop.
+	boolean keepRunning = true;
+
+	// Use the first argument as the directory name.
+	String dirName = args[0];
+
+	// We build up our Word Sense magic
+	WordSenseTrainer wordSense;
+
+	try{
+	    wordSense = new WordSenseTrainer(dirName);
+	} catch(Exception e) {
+	    e.printStackTrace();
+	    error("File not found, or file not a directory!");
+	    return;
+	}
+
+	while(keepRunning) {
+	    // We read in the user's input.
+
+	    keepRunning = false;
+	}
+    }
+
+    public static void error(String message) {
+	System.out.println("____________________________________");
+	System.out.println(message);
+	System.out.println("____________________________________");
+    }
 }
---
+++ run_test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+src=java
+java -cp $src WordSensor TestData
